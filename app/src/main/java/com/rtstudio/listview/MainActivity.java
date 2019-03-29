405e89d9b17fe628f96916c101f1ef883865bf9c
Raphael Rodrigues de Oliveira <rodrigues.rph@gmail.com>
2019-03-29 01:32:25 -0300

    Adicionado as listas e o layout

--- app/src/main/java/com/rtstudio/listview/MainActivity.java
+++ app/src/main/java/com/rtstudio/listview/MainActivity.java
@@ -1,13 +1,59 @@
 package com.rtstudio.listview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listaEstados;
+    private String nomesEstados[] = {
+            "Acre", "Alagoas", "Amapá",
+            "Amazonas", "Bahia", "Ceará",
+            "Distrito Federal", "Espírito Santo",
+            "Goiás", "Maranhão", "Mato Grosso",
+            "Mato Grosso do Sul", "Minas Gerais",
+            "Pará", "Paraíba", "Paraná", "Pernambuco",
+            "Piauí", "Rio de Janeiro", "Rio Grande do Norte",
+            "Rio Grande do Sul", "Rondônia", "Roraima",
+            "Santa Catarina", "São Paulo", "Sergipe",
+            "Tocantins"
+    };
+
+    private String[] estados = {
+            "AC", "AL", "AP", "AM", "BA",
+            "CE", "DF", "ES", "GO", "MA",
+            "MT", "MS", "MG", "PA", "PB",
+            "PR", "PE", "PI", "RJ", "RN",
+            "RS", "RO", "RR", "SC", "SP",
+            "SE", "TO",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaEstados = findViewById(R.id.listaEstadosId);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                estados
+        );
+
+        listaEstados.setAdapter(adapter);
+
+        listaEstados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, nomesEstados[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
